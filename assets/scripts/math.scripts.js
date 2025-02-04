4b8645101cac3bb23b159348259f266dca8c4e87
Gabriela Fernanda <gabriela.f.barbosa@aluno.senai.br>
2025-02-04 10:28:12 -0300

    feat: update calculate function

--- assets/scripts/math.scripts.js
+++ assets/scripts/math.scripts.js
@@ -1,12 +1,49 @@
-function calcular () {
-    let numA = Number (document.getElementById("numA").value);
-    let numB = Number (document.getElementById("numB").value);
+//function calcular () {
+    //let numA = Number (document.getElementById("numA").value);
+    //let numB = Number (document.getElementById("numB").value);
 
     //alert(numA);
     //alert(numB);
 
     //Soma
+    //let result = numA + numB 
+
+    //document.getElementById("result").innerText = result;
+//}
+
+function soma () {
+    let numA = Number (document.getElementById("numA").value);
+    let numB = Number (document.getElementById("numB").value);
+
+
     let result = numA + numB 
+
+    document.getElementById("result").innerText = result;
+}
+function sub () {
+    let numA = Number (document.getElementById("numA").value);
+    let numB = Number (document.getElementById("numB").value);
+
+
+    let result = numA - numB 
+
+    document.getElementById("result").innerText = result;
+}
+function mult () {
+    let numA = Number (document.getElementById("numA").value);
+    let numB = Number (document.getElementById("numB").value);
+
+
+    let result = numA * numB 
+
+    document.getElementById("result").innerText = result;
+}
+function div () {
+    let numA = Number (document.getElementById("numA").value);
+    let numB = Number (document.getElementById("numB").value);
+
+
+    let result = numA / numB 
 
     document.getElementById("result").innerText = result;
 }
